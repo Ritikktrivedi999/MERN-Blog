7242e97e8955c0f490cc77553e3fe9b47014be14
Ritikktrivedi999 <ritikktrivedi9999@gmail.com>
2024-02-24 02:39:48 +0530

    created header section with tailwind and Flowbite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Signin from "./Pages/Signin"
 import SignUp from "./Pages/SignUp"
 import Dashboard from "./Pages/Dashboard"
 import Projects from "./Pages/Projects"
+import Header from "./components/Header"
 export default function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -16,7 +18,6 @@ export default function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/projects" element={<Projects />} />
-
         </Routes>
       </BrowserRouter>
     </>
