3e55eedf01628d54e9a19b266d76abc06dea50fb
Ritikktrivedi999 <ritikktrivedi9999@gmail.com>
2024-02-24 14:03:06 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
+
 const app = express();
 
 app.listen(3000, () => {
