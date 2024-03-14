34fda1d1f52779875f3000a61ee7558e1f9b4ba6
Ritik Trivedi <ritikktrivedi9999@gmail.com>
2024-03-15 01:03:56 +0530

    add functionality to sign up page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     return (
         <Navbar className='border-b-2'>
             <Link to="/" className='self-crnter whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Ritik`s</span>Blog
+                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Ritik`s</span> Blog
             </Link>
             <form>
                 <TextInput
