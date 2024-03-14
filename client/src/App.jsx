7e4f7b8220550b48614978f34fdbf3316ea8ce24
Ritik Trivedi <ritikktrivedi9999@gmail.com>
2024-03-15 01:33:07 +0530

    build footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./Pages/SignUp"
 import Dashboard from "./Pages/Dashboard"
 import Projects from "./Pages/Projects"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 export default function App() {
   return (
     <>
@@ -19,6 +20,7 @@ export default function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
