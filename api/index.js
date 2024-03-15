9053ba2da060cd2726c2160704ee58b3225b3972
Ritik Trivedi <ritikktrivedi9999@gmail.com>
2024-03-15 22:29:25 +0530

    redux toolkit added

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 mongoose
---
+++ client/src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import useReducer from "./user/userSlice";
+export const store = configureStore({
+  reducer: {
+    user: useReducer,
+  },
+});
